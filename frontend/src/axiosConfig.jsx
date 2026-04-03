5f93372ec28c18011672f0c86e218c22a8b8c09e
Tirthesh Mehta <mehtatirthesh001@gmail.com>
2026-04-04 08:43:55 +1000

    Fronted Updation

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,23 +1,7 @@
-import axios from 'axios';
-import { API_BASE_URL } from './config/api';
-import { notifyUnauthorized } from './utils/authSession';
+import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://3.107.177.44:3333",
-  headers: { 'Content-Type': 'application/json' },
+const API = axios.create({
+  baseURL: "http://3.107.177.44:3333"
 });
 
-axiosInstance.interceptors.response.use(
-  (res) => res,
-  (error) => {
-    const status = error.response?.status;
-    const authHeader =
-      error.config?.headers?.Authorization ?? error.config?.headers?.authorization;
-    if (status === 401 && authHeader) {
-      notifyUnauthorized();
-    }
-    return Promise.reject(error);
-  }
-);
-
-export default axiosInstance;
+export default API;
